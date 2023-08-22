460acb4acc6d2330adb7e93cb284f28529c9b431
fujisawa <fujisawa5523@gmail.com>
2023-08-22 19:51:19 +0900

    Add items for testing

---
+++ src/cartItems.jsx
@@ -0,0 +1,32 @@
+const cartItems = [
+  {
+    id: 1,
+    title: "アヴァンタドール",
+    price: "3000000",
+    img: "/images/aventador.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "ガヤルド",
+    price: "2600000",
+    img: "/images/gallardo.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "ウラカン",
+    price: "2400000",
+    img: "/images/huracan.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "ムルシエラゴ",
+    price: "1800000",
+    img: "/images/murcielago.jpg",
+    amount: 1,
+  },
+];
+
+export default cartItems;
