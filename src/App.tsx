21aaa278f498e6a2e6cdf78bdbfab81b4b9d9260
fujisawa <fujisawa5523@gmail.com>
2023-08-29 21:08:09 +0900

    Add change to calculateTotals with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import './App.css';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
+import { RootState } from './store';
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state: RootState) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cartItems]); //[cartItems, dispatch];
+
   return (
     <main>
       <Navbar />
